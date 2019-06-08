dd3387eb0584f61b27585fa629447308d4b8623a
Imtiaz Epu <imtiaz@pop-os.localdomain>
2019-06-08 18:46:13 +0600

    install & config tailwind css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +13,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.postCss('resources/css/main.css', 'public/css', [
+    tailwindcss('./tailwind.js'),
+])
+.browserSync('tailwind-twitter.devops');
+
